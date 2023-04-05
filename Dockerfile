4db200737f2988e212dcbeb21072f8676c5b26b6
mhp <807753754@qq.com>
2023-04-05 09:53:34 +0800

    测试代码编写完成

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.7
+RUN pip install -r requirements.txt
+
+ENV PORT=程序端口号
+ENV WEROBOT_TOKEN=微信公众号的TOKEN
+
+CMD ["python", "app.py"]
---
+++ app.py
@@ -0,0 +1,11 @@
+import werobot
+
+robot = werobot.WeRoBot(token='tokenhere')
+
+
+@robot.text
+def hello_world():
+    return 'Hello World!'
+
+
+robot.run(port=80)
