730b9d7fa366d38722d13328cc148040cde51382
mhp <807753754@qq.com>
2023-04-05 19:20:57 +0800

    验证环境变量

--- app.py
+++ app.py
@@ -14,7 +14,9 @@ logger.addHandler(stream_handler)
 token = os.environ.get("WEROBOT_TOKEN", "")
 port = os.environ.get("PORT", 0)
 if token != "" and port != 0:
-    logger.error('T环境变量读取成功')
+    logger.log('环境变量读取成功')
+    logger.log("端口是{}".format(port))
+    logger.log("token是{}".format(token))
 
 else:
     logger.error('读取环境变量失败')
@@ -27,4 +29,4 @@ def hello_world():
     return 'Hello World!'
 
 
-robot.run(port=port)
+robot.run(port=port,host="0.0.0.0")
